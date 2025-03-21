72c624559dd496325f333b2dd2ccb120b42042e0
trudypainter <trudy.e.painter@gmail.com>
2025-03-20 23:06:56 -0400

    footer

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+HIIIIII
+THIS IS MY WEBSITE
+
+TRUDY.COMPUTER
+
+---
+
 # Clean Next.js + Sanity app
 
 This template includes a [Next.js](https://nextjs.org/) app with a [Sanity Studio](https://www.sanity.io/) – an open-source React application that connects to your Sanity project’s hosted dataset. The Studio is configured locally and can then be deployed for content collaboration.
--- nextjs-app/app/components/CustomFooter.tsx
+++ nextjs-app/app/components/CustomFooter.tsx
@@ -5,8 +5,47 @@ import { useEffect, useState } from "react";
 export const CustomFooter = () => {
   return (
     <footer className="container mx-auto px-4 py-8 max-w-content">
-      <div className="text-sm text-gray-200">
-        <p>© {new Date().getFullYear()} Trudy Painter</p>
+      <div className="flex justify-between">
+        <div className="text-sm text-gray-200">
+          <p>© {new Date().getFullYear()} Trudy Painter</p>
+        </div>
+        <div className="text-sm text-gray-200">
+          <p>
+            Coded by me + Claude / Cursor (
+            <a
+              href="https://github.com/trudypainter/clean-sanity"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline hover:text-gray-400 transition-colors"
+            >
+              source
+            </a>
+            )
+          </p>
+        </div>
+      </div>
+      <div className="text-sm font-cursive my-12 text-gray-200 mx-auto w-full text-center">
+        <div>♡ Thanks for scrolling</div>
+        <div>
+          If you made it this far, we probably have some shared interests.{" "}
+          <br></br>Feel free to check out my{" "}
+          <a
+            href="https://www.are.na/trudy-painter"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline hover:text-gray-400 transition-colors"
+          >
+            Are.na
+          </a>{" "}
+          or say{" "}
+          <a
+            href="mailto:hello@trudy.computer"
+            className="underline hover:text-gray-400 transition-colors"
+          >
+            hello@trudy.computer
+          </a>
+          .
+        </div>
       </div>
     </footer>
   );
